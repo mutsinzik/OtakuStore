b2bc01ce9d19430995d469e2f36759bf57587e6c
Mutsinzi_k <kabutobill6@gmail.com>
2023-02-08 15:23:00 +0200

    Added Imagefield and added product images

--- weeb_store/weeb_store/settings.py
+++ weeb_store/weeb_store/settings.py
@@ -143,3 +143,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_URL = '/images/'
